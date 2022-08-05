b03240cf7dc549e3239a2e9b833391d270b6f763
leofracca <leonardofraccaroli1999@gmail.com>
2022-08-05 17:48:54 +0200

    Added graphics and CMakeLists

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include "Chip8.h"
+#include "Platform.h"
+#include <chrono>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	if (argc != 4)
+	{
+		std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
+		std::exit(EXIT_FAILURE);
+	}
+
+	int videoScale = std::stoi(argv[1]);
+	int cycleDelay = std::stoi(argv[2]);
+	char const* romFilename = argv[3];
+
+	Platform platform("CHIP-8 Emulator", SCREEN_WIDTH * videoScale, SCREEN_HEIGHT * videoScale, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	Chip8 chip8;
+	chip8.loadGame(romFilename);
+
+	int videoPitch = sizeof(chip8.display[0]) * SCREEN_WIDTH;
+
+	auto lastCycleTime = std::chrono::high_resolution_clock::now();
+	bool quit = false;
+
+	while (!quit)
+	{
+		quit = platform.ProcessInput(chip8.keypad);
+
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
+
+		if (dt > cycleDelay)
+		{
+			lastCycleTime = currentTime;
+
+			chip8.cycle();
+
+			platform.Update(chip8.display, videoPitch);
+		}
+	}
+
+	return 0;
+}
